be24a03fcdb9a5ccedfcdea8df649609f287c2ad
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-04-03 00:32:11 +0600

    picture changed

--- index.pack.js
+++ index.pack.js
@@ -527,7 +527,7 @@ function Header() {
     return (
 
         //<header><h1>This is test Body</h1></header>
-        _react2.default.createElement("img", { src: "https://scontent.fdac145-1.fna.fbcdn.net/v/t39.30808-6/328811966_616559640278489_6401651324655420191_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=X-bX7DNDY0oAX9yB-RM&tn=WJLKE0006Rf5MFtk&_nc_ht=scontent.fdac145-1.fna&oh=00_AfAlVZEEcd96M4QAdU1eHGO3WzRQF1q1ho7slGc3pvOJqQ&oe=63ED6A64", height: "317px", width: "317px", alt: "profile-pic" })
+        _react2.default.createElement("img", { src: "https://avatars.githubusercontent.com/u/61237769?v=4", height: "317px", width: "317px", alt: "profile-pic" })
     );
 }
 
