c7fff9fca92cc4ad25f0fae93c783833561c1193
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-11-09 09:41:38 +0600

    animation added+text me updated

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,6 +1,11 @@
 const AboutMe = () => {
     return ( 
-        <div id="about-me" className="w-full flex flex-col my-8 justify-center items-center min-h-[80svh]">About Me</div>
+        <div id="about-me" className="w-10/12 flex flex-between my-8  justify-center items-center min-h-[80svh]">
+            <div className="flex flex-col w-full justify-between gap-8">
+                <h4 className="text-bold text-xl font-mono">BUT WHO IS SHIUM?</h4>
+                <h3 className="text-4xl font-extrabold">Get to know me!</h3>
+            </div>
+        </div>
      );
 }
  
--- app/text-me/page.tsx
+++ app/text-me/page.tsx
@@ -1,26 +1,50 @@
-import Link from "next/link";
 import Image from "next/image";
 import WhatsappIcon from "../../public/whatsapp.png"
+import GMailIcon from "../../public/Gmail.png"
 
 const TextMe = () => {
     // Pre-filled message for WhatsApp
     const message = "Hello, I have viewed your portfolio and would like to get in touch with you. Please let me know how we can proceed.";
-
-    // URL-encode the message
+    const subject = "I would like to talk to you";
+    
+    // URL-encode the message and subject
     const encodedMessage = encodeURIComponent(message);
+    const encodedSubject = encodeURIComponent(subject);
 
     return ( 
-        <div id="text-me" className="min-h-[80vh]">
-            <Link href={`https://wa.me/8801729771453?text=${encodedMessage}`} passHref target="_blank" rel="noopener noreferrer">
-                <Image 
-                    alt="Chat on WhatsApp" 
-                    src={WhatsappIcon} // Update with the correct path
-                    width={100} // Adjust width as necessary
-                    height={100} // Adjust height as necessary
-                />
-            </Link>
+        <div id="text-me" className="min-h-[80vh] w-10/12 flex items-center justify-center mt-12">
+            <div className="flex flex-col justify-between items-center gap-4">
+                <h4 className="font-mono text-xl font-bold">DONT&apos;T LIKE CALLS?</h4>
+                <h2 className="text-3xl font-extrabold">Text me! 👋</h2>
+                <p className="text-center mb-8">
+                    If booking calls with strangers from the internet is not your cup of tea ☕️, great news - you can always text or email me first!
+                </p>
+                <div className="flex justify-center w-6/12 items-center gap-8">
+                    <a href={`https://wa.me/8801729771453?text=${encodedMessage}`}>
+                        <Image 
+                            alt="Chat on WhatsApp" 
+                            src={WhatsappIcon} 
+                            width={40} 
+                            height={40} 
+                        />
+                    </a>
+                    <a 
+                        href={`mailto:officials.shium@gmail.com?subject=${encodedSubject}&body=${encodedMessage}`} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <Image 
+                            alt="Gmail" 
+                            src={GMailIcon} 
+                            width={40} 
+                            height={40} 
+                        />
+                    </a>
+                </div>
+                
+            </div>
         </div>
     );
 }
- 
+
 export default TextMe;
